c9ecde108f991323545ddb55bac9c22248dca7f3
Elmar Athmer <elmar@athmer.org>
2016-03-18 00:59:18 +0100

    create instance for authorized api

--- twitter.py
+++ twitter.py
@@ -1,6 +1,10 @@
 from twython.api import Twython
 
 
+class AuthorizedTwitter:
+    def __init__(self, consumer_token, consumer_secret, access_token, secret):
+        self.t = Twython(consumer_token, consumer_secret, access_token, secret)
+
 class Twitter:
     def __init__(self, consumer_token, consumer_secret):
         self.t = Twython(consumer_token, consumer_secret)
@@ -16,3 +20,6 @@ class Twitter:
         twitter = Twython(self.consumer_token, self.consumer_secret, auth['oauth_token'], auth['oauth_token_secret'])
         tokens = twitter.get_authorized_tokens(oauth_verifier=verifier)
         return tokens['oauth_token'], tokens['oauth_token_secret']
+
+    def authenticate(self, access_token, secret):
+        return AuthorizedTwitter(self.consumer_token, self.consumer_secret, access_token, secret)
